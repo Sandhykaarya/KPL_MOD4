2cfd02a51cebf177d2d35dffdb9b258a0aa077d2
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-03-19 16:52:17 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace modul4_1302204002
     {
         static void Main(string[] args)
         {
-            
+            SimpleDataBase<float> sd = new SimpleDataBase<float>();
+
+            sd.simpleDataBase();
+            sd.addNewData(13);
+            sd.addNewData(02);
+            sd.addNewData(20);
+            sd.printAllData();
+        }
+    }
+
+    public class SimpleDataBase<T>
+    {
+        public List<T> storedData;
+        public List<DateTime> inputDates = new List<DateTime>();
+        int i = -1;
+
+        public void simpleDataBase()
+        {
+            storedData = new List<T>();
+        }
+
+        public void addNewData(T data)
+        { 
+            i++;
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData()
+        {
+            for(int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i] + " yang disimpan pada waktu " + inputDates[0]);
+            }
         }
     }
 }
